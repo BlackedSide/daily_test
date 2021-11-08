39c64072ddecbc286aee481a00fac1ff8c0a47fb
jinpeng.fan <jinpeng.fan@qunar.com>
2021-11-08 17:20:41 +0800

    测试递归寻找Git工程根路径与编译产物

--- maven_test/src/test/java/GitTest.java
+++ maven_test/src/test/java/GitTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * Created by funny
@@ -18,12 +19,55 @@ import java.io.IOException;
 public class GitTest {
     @Test
     public void config() throws IOException {
-        String path = "/Users/funny/Qunar/Github/daily_test";
-        String path2 = "/Users/funny/Qunar/Basic_Platform/spider";
+        String path = "/Users/fanny/Qunar/Github/daily_test";
+        String path2 = "/Users/fanny/Qunar/Basic_Platform/spider";
         File file = new File(path);
         StoredConfig config = Git.open(file).getRepository().getConfig();
         Repository repository = Git.open(file).getRepository();
         System.out.println(repository.getDirectory().getParent());
         System.out.println(config.getString("user", null, "user"));
+    }
+
+    @Test
+    public void resolveAll() throws IOException {
+        File file = new File("/Users/fanny/Qunar/Basic_Platform/h_n_callcenter/callcenter-flow/flow-dao");
+        File rootFile = getProjectRoot(file, 0);
+        System.out.println(rootFile);
+        recurse(rootFile, 0);
+    }
+
+    private static void recurse(File rootFile, int counting) {
+        if (counting >= recurseDepth + 1) {
+            return;
+        }
+        if (rootFile.exists() && rootFile.isDirectory()) {
+            ++counting;
+            for (File subModule : Objects.requireNonNull(rootFile.listFiles())) {
+                if (subModule.getName().equals("target") && subModule.isDirectory()) {
+                    for (File output : Objects.requireNonNull(subModule.listFiles())) {
+                        if (output.getName().equals("classes") && output.isDirectory()) {
+                            System.out.println(rootFile.getName() + "->" + subModule.getName() + "->" + output.getName());
+                        }
+                    }
+                } else {
+                    recurse(subModule, counting);
+                }
+            }
+        }
+    }
+
+    private final static int recurseDepth = 5;
+
+    private static File getProjectRoot(File baseDir, int count) {
+        try {
+            ++count;
+            String rootPath = Git.open(baseDir).getRepository().getDirectory().getParent();
+            return new File(rootPath);
+        } catch (Exception e) {
+            if (count >= recurseDepth) {
+                throw new RuntimeException("无法解析Git Repository根路径");
+            }
+            return getProjectRoot(baseDir.getParentFile(), count);
+        }
     }
 }
